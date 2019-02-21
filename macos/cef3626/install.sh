97781a19f21a15a79bf6e25655338884adade986
tslearn <tslearn@163.com>
2019-02-21 15:22:38 +0800

    Update install.sh

--- macos/cef3626/install.sh
+++ macos/cef3626/install.sh
@@ -21,7 +21,7 @@ if [ ! -d ${CEFPATH} ]; then
     cd ${CEFPATH}
 
     # download
-    curl http://opensource.spotify.com/cefbuilds/cef_binary_3.3626.1886.g162fdec_macosx64.tar.bz2 -o cef_binary.tar.bz2
+    curl http://opensource.spotify.com/cefbuilds/cef_binary_3.3626.1891.g52be333_macosx64.tar.bz2 -o cef_binary.tar.bz2
     tar -xf cef_binary.tar.bz2
 
     # build
